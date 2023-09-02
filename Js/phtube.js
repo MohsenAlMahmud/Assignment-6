209b9d052b367d53070028869e5a8eeb853e5d30
MohsenAlMahmud <mohsen.al.mahmud@gmail.com>
2023-09-02 19:12:38 +0600

    final revision complete

--- Js/phtube.js
+++ Js/phtube.js
@@ -16,28 +16,26 @@ const loadVideos = async () => {
     }
 
 };
-let sortedByViews = false;
+
 const loadVideosHandler = async (categoryId) => {
     
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
     const data = await response.json();
-    // if(sortedByViews){
-    //     data.data.sort((a,b) =>b.other.views - a.other.views);        
-    // }
+    
     const cardContainer = document.getElementById('card-container');
     cardContainer.innerHTML = '';
 
     if (data.data.length === 0) {
         const div = document.createElement('div');
         div.innerHTML = `
-        <div class="card bg-gray-100 w-full md:mx-[150px] md:my-64 mt-6">
+        <div class="card bg-gray-100 w-full md:mx-[150px] lg:mx-[650px] md:my-64 mt-6">
         <div class="">
             <img class="mx-28" src="./Icon.png" alt="">
             <h2 class="text-center text-black text-3xl font-bold">Oops!! Sorry, There is no <br> content here</h2>
         </div>
         </div>
         `;
-        cardContainer.appendChild(div);        
+        cardContainer.appendChild(div);       
     }
     else {
         data.data.forEach((videos) => {
@@ -48,8 +46,7 @@ const loadVideosHandler = async (categoryId) => {
                 <figure class="position: relative w-full h-56  rounded-md shadow-xl ">
                 <img class="w-full h-full" src="${videos.thumbnail}" />
                 </figure>
-                <p class="text-white bg-gray-950 position: absolute w-fit rounded-md ml-36 mt-48 px-1">${(Math.round(videos.others.posted_date / 60) - Math.round(videos.others.posted_date / 60) % 60) / 60} hrs   ${Math.round(videos.others.posted_date / 60) % 60} mins ago </p>  
-                
+                <p class="text-white bg-gray-950 position: absolute w-fit rounded-md ml-36 mt-48 px-1">${(Math.round(videos.others.posted_date / 60) - Math.round(videos.others.posted_date / 60) % 60) / 60} hrs   ${Math.round(videos.others.posted_date / 60) % 60} mins ago </p>
                 <div class="card-body">
                     <div class="flex p-3 gap-6">
                         <div>
@@ -76,9 +73,3 @@ const blogPage = () =>{
     const blogHtmlFile = 'blog.html';
     window.open(blogHtmlFile);
 }
-const toggleSortByViews = () => {
-    sortedByViews = !sortedByViews;
-    const currentCategory = document.getElementById();
-    
-}
-// Finish
